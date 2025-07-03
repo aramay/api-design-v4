7775d19a25e8dd03994142875e31ac38990d5697
aramay <kasper_341@hotmail.com>
2025-07-02 21:13:55 -0700

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express from "express";
+import router from "./router";
+import morgan from "morgan";
 // const path = require("path");
 
 const PORT = 3000;
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     console.log("hello world");
     res.json({message: "hello world"})
     // res.send("hell world");
 })
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port ${PORT}`)
