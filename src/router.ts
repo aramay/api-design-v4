b3bb4022ef4b9b5c0d3732c63e5661416f4edec6
aramay <kasper_341@hotmail.com>
2025-07-02 19:05:49 -0700

    Added routes for Products, Updates, UpdatePoints.

--- src/router.ts
+++ src/router.ts
@@ -1,1 +1,35 @@
-import { Router } from "express";
+import { Router } from "express"
+
+const router = Router()
+
+/**
+ * Product
+ */
+
+router.get("/products", () => {})
+router.get("/product/:id", () => {})
+router.post("/product", () => {})
+router.put("/product/:id", () => {})
+// patch only updates fields
+// router.patch("/product/:id", () => {}) 
+router.delete("/product/:id", () => {})
+
+/**
+ * Updates
+ */
+
+router.get("/updates", () => {})
+router.get("/update/:id", () => {})
+router.post("/update", () => {})
+router.put("/update/:id", () => {})
+router.delete("/update/:id", () => {})
+
+/**
+ * Updates Points
+ */
+
+router.get("/updatespoints", () => {})
+router.get("/updatepoint/:id", () => {})
+router.post("/updatepoint", () => {})
+router.put("/updatepoint/:id", () => {})
+router.delete("/updatepoint/:id", () => {})
